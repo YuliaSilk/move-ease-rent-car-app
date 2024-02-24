d3042b6807544e789dcce72b834ca3c825810c8c
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-24 17:01:37 +0200

    first commit: check deploy; instalation libraries needs to work wiith rpogect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from 'components/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+
+const theme = {
+  colors: {
+    main: '#3470FF',
+    header: '#153834',
+    backgraund: '#fff',
+    active:'#c70e38',
+    colorwerbs:'#121417',
+    input:'#edb234',
+    acent:'#0B44CD',
+  },
+  spacing: value => `${value*4}px`,
+  radius: {
+    m: '12px',
+  },
+}
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename="move-ease-rent-car-app">
+    <Provider >
+    <PersistGate loading={null}>
+    <ThemeProvider theme={theme}>
     <App />
+    <GlobalStyle/>
+    </ThemeProvider>
+    </PersistGate>
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/FavoritesPage/FavoritesPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomePage = (props) => {
+    return (
+        <>
+        <p>Hi! I'm FavoritePage</p>
+        </>
+    )
+}
