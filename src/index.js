37f9de85cd718944abd21140745bece3f62ccedf
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-25 22:30:34 +0200

    fix some params

--- src/index.js
+++ src/index.js
@@ -13,28 +13,28 @@ const theme = {
     main: '#3470FF',
     header: '#153834',
     backgraund: '#fff',
-    active:'#c70e38',
-    colorwerbs:'#121417',
-    input:'#edb234',
-    acent:'#0B44CD',
+    active: '#c70e38',
+    colorwerbs: '#121417',
+    input: '#edb234',
+    acent: '#0B44CD',
   },
-  spacing: value => `${value*4}px`,
+  spacing: value => `${value * 4}px`,
   radius: {
     m: '12px',
   },
-}
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='move-easily-rent-car-app'>
-    <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-    <ThemeProvider theme={theme}>
-    <App />
-    <GlobalStyle/>
-    </ThemeProvider>
-    </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <App />
+            <GlobalStyle />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
